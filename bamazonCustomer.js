6e20ca5def4beb2230a53f8873a281c4248eff23
Christopher Hayashi <Chrisrhayashi@gmail.com>
2019-12-27 16:39:20 -0800

    displayed table of all entries in database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,16 +2,35 @@ var mysql = require("mysql");
 
 var inquirer = require("inquirer");
 
+var Table = require("cli-table");
+// var table = new Table({
+//     head: ["ID", "Product", "Department", "Price", "Stock"]
+// });
+
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "Localhost",
     port: 3306,
-    username: "root",
+    user: "root",
     password: "password",
     database: "bamazon"
 });
 
-connection.connect(function(error) {
-    if (error)
-        throw error;
+connection.connect(function(err) {
+    if (err) throw err;
 });
 
+connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    var table = new Table({
+        head: ["ID", "Product", "Department", "Price", "Stock"]
+    });
+
+    for (var i = 0; i < res.length; i++) {
+        table.push(
+            [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+        );
+    }
+
+    console.log(table.toString());
+});
